c9d896648a7c0717761186c6d9e6a4456adde234
DiscoFerry <74020155+DiscoFerry@users.noreply.github.com>
2022-08-18 23:31:06 +0100

    dropdown working

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,15 @@
 import './App.css';
 import React from "react";
-import NewComponent from './NewComponent';
+import TeamsDropdown from './TeamsDropdown';
 
 function App() {
-  const [data, setData] = React.useState(null);
   const [count, setCount] = React.useState(0);
-
-  React.useEffect(() => {   
-    async function getData() {
-      let response = await fetch("/team/tottenham");
-      response = await response.json();
-      console.log(response.data);
-      setData(response.data.Team);
-    }
-    getData();
-  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
         <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
-        <NewComponent />
+        <TeamsDropdown />
       </header>
     </div>
   );
---
+++ client/src/TeamsDropdown.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React from "react";
+
+function TeamsDropdown() {
+    const [currentTeam, setCurrentTeam] = React.useState(null);
+    const [teams, setTeams] = React.useState(null);
+
+    function handleTeam(team, index){
+        setCurrentTeam(team.target.value);
+    }
+
+    React.useEffect(() => {   
+        async function getAllTeams() {
+            let response = await fetch("/allTeamsList");
+            console.log(response);
+            response = await response.json();
+            console.log(response.data);
+            console.log(response.data[0].Team);
+            setTeams(response.data);
+        }
+        getAllTeams();
+    }, []);
+
+    if (!teams) return; //if api data hasnt loaded yet dont send component
+
+    return(
+        <div>
+            <select onChange={handleTeam}>
+                {teams.map((team) => <option key={team.Team} value={team.Team}>{team.Team}</option>)}
+            </select>
+            <br />
+            {currentTeam ? currentTeam : "Select a Team"}
+        </div>
+    );
+}
+export default TeamsDropdown;
