7e764bd3ff3375bbfb4f9dd01e27518904d3f38c
DiscoFerry <74020155+DiscoFerry@users.noreply.github.com>
2022-08-23 00:50:18 +0100

    stat table can be sorted and reversed by clicking on headers

--- client/src/StatTable.js
+++ client/src/StatTable.js
@@ -2,40 +2,57 @@ import './App.css';
 import React from "react";
 
 function PredictGame() {
-    //const [sortingStat, setSortingStat] = React.useState("ELO");
+    const [sortingStat, setSortingStat] = React.useState("ELO");
     const [teams, setTeams] = React.useState(null);
 
     React.useEffect(() => {   
         async function getAllTeams() {
-            let response = await fetch("/sorted/ELO");
+            let response = await fetch(`/sorted/ELO`);
             response = await response.json();
             setTeams(response.data);
         }
         getAllTeams();
     }, []);
 
-    if (!teams) return <p>Loading...</p>; //if api data hasnt loaded yet dont send component
+    async function sortBy(stat) {
+        if (stat === sortingStat) {
+            reverseTable();
+            return;
+        }
+        setSortingStat(stat);
+        let response = await fetch(`/sorted/${stat}`);
+        response = await response.json();
+        setTeams(response.data);
+    }
 
-    console.log(teams);
+    async function reverseTable() {
+        let teamsArray = [...teams]; //create new array instead of mutating old one to force re render
+        let teamsReversed = await teamsArray.reverse();
+        console.log(teamsReversed);
+        setTeams(teamsReversed);
+    }
+
+    if (!teams) return <p>Loading...</p>; //if api data hasnt loaded yet dont send component
 
     return (
         <div>
-            <h2>Sorted by ELO</h2>
+            <h2>Sorted by {sortingStat}</h2> 
+            <button onClick={reverseTable}>Reverse Order</button>
             <table>
                 <tbody>
                     <tr>
                         <th>Position</th>
-                        <th>Team</th>
-                        <th>Elo</th>
-                        <th>Wins</th>
-                        <th>Losses</th>
-                        <th>Draws</th>
-                        <th>Goals Scored</th>
-                        <th>Goals Conceded</th>
+                        <th onClick={() => sortBy("Team")}>Team</th>
+                        <th onClick={() => sortBy("ELO")}>Elo</th>
+                        <th onClick={() => sortBy("Wins")}>Wins</th>
+                        <th onClick={() => sortBy("Losses")}>Losses</th>
+                        <th onClick={() => sortBy("Draws")}>Draws</th>
+                        <th onClick={() => sortBy("GoalsFor")}>Goals Scored</th>
+                        <th onClick={() => sortBy("GoalsAgainst")}>Goals Conceded</th>
                     </tr>
                     {teams.map((team, index) => <tr key={team.Team}>
                         <th>{index + 1}</th>
-                        {Object.keys(team).map((stat) => <td>{stat === "ELO" ? team[stat].toFixed(2) : team[stat]}</td>)}
+                        {Object.keys(team).map((stat) => <td key={stat}>{stat === "ELO" ? team[stat].toFixed(2) : team[stat]}</td>)}
                     </tr>)}
                 </tbody>
             </table>
